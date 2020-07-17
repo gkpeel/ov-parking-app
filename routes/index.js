bff012d9c5e5780294544dee35bf1a9c9935961f
gkpeel <gkpeel@gmail.com>
2020-07-17 11:47:09 -0400

    initial server setup, basic route creation

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+// @desc    Show Parking registry for that day
+// @route   GET /
+router.get('/', (req, res) => {
+	res.send('hello world - main');
+});
+
+// @desc    Show Dashboard of Users owned Parking Spots
+// route    GET /dashboard
+router.get('/dashboard', (req, res) => {
+	res.send('hello world - dashboard');
+});
+
+module.exports = router;
